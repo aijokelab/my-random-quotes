509a3bef3487ada96a0d3b897e14d29ec8dfc32c
aijokelab <128240538+aijokelab@users.noreply.github.com>
2023-03-19 00:36:42 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,18 @@ const quotes = [
     "「成功は最善の復讐である」 - フランク・シナトラ",
     "「明日を変えるには、今日を変えなさい」 - パウロ・コエーリョ",
     "「失敗は成功への第一歩である」 - トーマス・エジソン",
-    // 以下、さらに言葉
+    // 以下、さらに言葉や格言を追加していく
+];
+
+function displayRandomQuote() {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    const randomQuote = quotes[randomIndex];
+    document.getElementById("quote").innerText = randomQuote;
+}
+
+document.getElementById("generate").addEventListener("click", displayRandomQuote);
+
+// 最初の格言を表示
+displayRandomQuote();
+
 
